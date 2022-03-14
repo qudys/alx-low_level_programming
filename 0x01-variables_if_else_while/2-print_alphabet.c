2c60a27f0d9f0c9ab29d7ef6d1f8ccd7e77cf283
qudys <shuaibqudus33@gmail.com>
2022-03-14 03:45:42 -0700

    print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	char z;
+
+	a = 'a';
+	z = 'z';
+
+	while (a <= z)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
