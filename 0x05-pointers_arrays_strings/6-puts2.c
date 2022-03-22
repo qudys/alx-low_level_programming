6a765286309eda8250a3a46520aac995dc091a98
qudys <shuaibqudus33@gmail.com>
2022-03-22 13:14:18 -0700

    print even indexed char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - print every other char
+ * @str: string input
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = strlen(str);
+	for (j = 0; j < i - 1; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
