c9fef5a09ef012e610ee36d1ab29a030149a302c
qudus <shuaibqudus33@gmail.com>
2022-04-03 07:11:35 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that adds input integers
+ * @argc: argument count
+ * @argv: array of string arguments
+ * Return: 0 if success and 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	sum = 0;
+	j = 0;
+	if (argc > 2)
+	{
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i] != 0 && atoi(argv[i]) == 0)
+		{
+			j = 1;
+			break;
+		}
+		sum += atoi(argv[i]);
+	}
+	if (j == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", sum);
+		return (0);
+	}
+	}
+	else
+		printf("0\n");
+	return (0);
+}
