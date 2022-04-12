5614ef8a29896dd48f67f7f1b8faf537b642ea7b
qudus <shuaibqudus33@gmail.com>
2022-04-12 08:02:30 -0700

    ABS macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a  funtion macro named
+ *       ABS that computes the absolute value of the token.
+ */
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
