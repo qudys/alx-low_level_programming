3835e06921f0f3e19d90dd212955b42e235f64b8
qudus <shuaibqudus33@gmail.com>
2022-04-12 09:07:50 -0700

    SUM macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Header file that defines a macro named
+ *       SUM(x, y) as the sum of two arguments
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
