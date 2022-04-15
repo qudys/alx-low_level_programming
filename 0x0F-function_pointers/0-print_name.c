6c8d350924abcb9cbecfd833f2074726f23f0f69
qudus <shuaibqudus33@gmail.com>
2022-04-15 16:02:59 -0700

    printname

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
