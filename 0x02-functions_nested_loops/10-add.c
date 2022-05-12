00a8db1e855f16739bb98c4ae47a695dc316b5b3
qudus <shuaibqudus33@gmail.com>
2022-05-12 09:41:36 -0700

    add two integer inputs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - function that adds two integers
+ * @a: int a
+ * @b: int b
+ * Return: sum of the two integers
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
