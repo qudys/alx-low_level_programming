df055f67b2f73e478b42424e0973a254dc74c0cf
qudus <shuaibqudus33@gmail.com>
2022-05-23 08:07:44 -0700

    fizzbuzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,6 +9,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i != 100)
+		{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
@@ -26,6 +28,9 @@ int main(void)
 		}
 		else
 			printf("%d ", i);
+	}
+		else
+			printf("Buzz");
 	}
 	printf("\n");
 	return (0);
